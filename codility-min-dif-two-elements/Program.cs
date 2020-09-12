9e02323b0aeefb1fbcb8b881b939375e1a5736af
Ricardo Torres <ricardojavister@gmail.com>
2020-09-11 23:39:14 -0300

    first commit

---
+++ codility-min-dif-two-elements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace codility_min_dif_two_elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int[] vector)
+        {
+            int diff = int.MaxValue;
+            for (int i = 0; i < vector.Length - 1; i++)
+                for (int j = i + 1; j < vector.Length; j++)
+                    if (Math.Abs((vector[i] - vector[j])) < diff)
+                        diff = Math.Abs((vector[i] - vector[j]));
+
+            return diff;
+        }
+    }
+}
